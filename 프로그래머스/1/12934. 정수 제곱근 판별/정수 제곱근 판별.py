324ca617b94ecc462d4df6dde375808d25daba9a
HeoJunekang <junek0407@gmail.com>
2026-09-08 14:36:58 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,14 +1,7 @@
 def solution(n):
-    answer = 0
-    i = 1
-    while True:
-        if i*i == n:
-            answer = (i+1)*(i+1)
-            break
-        elif i*i > n:
-            answer = -1
-            break
-        else:
-            i+=1
-            continue
-    return answer
+    nn = n **(1/2)
+    if nn %1 ==0:
+        return (nn+1) **2
+    else:
+        return -1
+    
